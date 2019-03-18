b4f6dcb9d1b25ec1418e570f775f20b5f26a90c3
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-18 21:52:35 +0900

    implement to parse arguments

--- main.go
+++ main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func main() {
+	flag.Parse()
+	argv := flag.Args()
+	if len(argv) < 1 {
+		log.Fatal("insufficient arguments")
+	}
+
+	cmd := flag.Arg(0)
+
+	switch cmd {
+	case "add",
+		"cat-file",
+		"checkout",
+		"commit",
+		"hash-object",
+		"init",
+		"log",
+		"ls-tree",
+		"merge",
+		"rebase",
+		"rev-parse",
+		"rm",
+		"show-ref",
+		"tag":
+		command(cmd)
+	}
+}
+
+func command(cmd string) {
+	fmt.Printf("your input command is %s\n", cmd)
+}
